be0c6cc2db891c4ac82f18f97ca6ed4ce8560cbb
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-02-12 19:31:31 +0300

    Initial Commit

---
+++ HomeWork1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork1.2
+//
+//  Created by Paul on 12.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
