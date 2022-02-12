27f2b2bd039d8e6a9f0935cd2ae5b282f999b71f
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-02-12 20:40:03 +0300

    Traffic Light project's done! Partly and badly, waiting for updates and some improvements :)

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
-
+//MARK: Help me pls: Можно ли собрать картинку светофора и мои кружки в один стэк, чтобы можно было адаптировать его под любой экран?
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLighView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var changeColorBtnView: UIButton!
+    
+    
+    override func viewDidLoad() { //Если я понял правильно, эта функция, унаследованная от класса вью контроллер, предназначена для визуальных преобразований
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Setting corner radius for traffic lights
+        redLightView.layer.cornerRadius = 51
+        yellowLighView.layer.cornerRadius = 51
+        greenLightView.layer.cornerRadius = 51
+        
+        //Get the right colors alpha
+        redLightView.alpha = 1
+        yellowLighView.alpha = 0.1
+        greenLightView.alpha = 0.1
+        
+        //Setting corner radius for button
+        changeColorBtnView.layer.cornerRadius = 10
     }
-
-
+    @IBAction func changeColorBtnAct(_ sender: Any) {
+        if redLightView.alpha == 1 {
+            redLightView.alpha = 0.1
+            yellowLighView.alpha = 1
+        } else if yellowLighView.alpha == 1 {
+            yellowLighView.alpha = 0.1
+            greenLightView.alpha = 1
+        } else {
+            greenLightView.alpha = 0.1
+            redLightView.alpha = 1
+        }
+    }
 }
 
