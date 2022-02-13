083844e321ad6d50a2e7c34f60f5d33f454876e0
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-02-13 23:33:11 +0300

    I think that's fine :)

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -1,47 +1,89 @@
 import UIKit
+
+// Plan:
+// 1. IBOutlets
+// 2. Properties
+// 3. Life Cycle
+// 4. Methods
+// 5. Action
+
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
     
+    // MARK: - IBOutlets
     @IBOutlet weak var redLightView: UIView!
     @IBOutlet weak var yellowLightView: UIView!
     @IBOutlet weak var greenLightView: UIView!
     
     @IBOutlet weak var changeColorBtnView: UIButton!
+    // MARK: - Properties
+    let lightIsOff: CGFloat = 0.1
+    let lightIsOn: CGFloat = 1
     
-    override func viewDidLoad() { //Эта функция создана для работы с внешним видом эл-тов?
+    var currentLight: CurrentLight = .green
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        //Q: В этом методе мы работаем с внешним видом?
+        
+        /*
+         A: это метод жизненого цикла вьюконтроллера - ты о нем узнаешь по-моему
+         с 9 урока.
+         B нем ты работаешь действительно с внешним видом - потому что все элементы
+         загружаются c холста и только в этом методе они завершают загрузку и
+         действительно становятся доступными
+        */
+        
+        //Q: Спасибо :)
+        
         super.viewDidLoad()
-        
-        //Setting corner radius for our lights
-        redLightView.layer.cornerRadius = 38
-        yellowLightView.layer.cornerRadius = 38
-        greenLightView.layer.cornerRadius = 38
-        
-        //Setting alpha for lights
-        redLightView.alpha = 0.1
-        yellowLightView.alpha = 0.1
-        greenLightView.alpha = 0.1
-        
-        //Setting corner radius for button
-        changeColorBtnView.layer.cornerRadius = 10
+        setupUI()
     }
     
+    // MARK: - Methods
+    func setupUI() {
+        setupColorForView()
+    }
+    
+    func setupColorForView() {
+        
+        // Making circles out of squares
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        
+        changeColorBtnView.layer.cornerRadius = 10
+        
+        redLightView.alpha = lightIsOff
+        yellowLightView.alpha = lightIsOff
+        greenLightView.alpha = lightIsOff
+    }
+    
+
+    // MARK: - Actions
+
     @IBAction func changeColorBtnAction(_ sender: Any) {
-        
-        changeColorBtnView.setTitle("Change color!", for: .normal)
-        
-        if redLightView.alpha == 1 {
+        switch currentLight {
             
-            redLightView.alpha = 0.1
-            yellowLightView.alpha = 1
+        case .red:
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+            currentLight = .yellow
             
-        } else if yellowLightView.alpha == 1 {
-            yellowLightView.alpha = 0.1
-            greenLightView.alpha = 1
+        case .yellow:
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+            currentLight = .green
             
-        } else {
-            greenLightView.alpha = 0.1
-            redLightView.alpha = 1
+        case .green:
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+            currentLight = .red
         }
     }
-    
 }
-
